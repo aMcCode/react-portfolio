3323fb7083ec42082ac3fc936b15400795506f38
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-10-16 00:26:11 -0400

    added logo, updated nav hamburger color on colapse, implemented time text update on hero

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Image }from "react-bootstrap";
 import '../index.css'
+import logo_bw from '../assets/portfolio_imgs/logo_bw.png'
 
 const Navigation = () => {
   return (
-    <Navbar className="navbar" expand="lg">
+    <Navbar bg='dark' expand="lg" variant='dark' >
       <Container>
-        <Navbar.Brand href="/">LOGO</Navbar.Brand>
+        <Navbar.Brand href="/"><Image className="heroImg" align="center" src={logo_bw} width={125} height={50} /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import 'bootstrap'
 import '../index.css'
 import heroBackground from '../assets/portfolio_imgs/grey_white_swirls.jpg'
 import { Container, Row, Col } from "react-bootstrap";
 
+const descriptions = [ 'clean', 'efficient', '#3' ]
+
 const Hero = () => {
+
+  const [newDescript, setNewDescript ] = useState("");
+
+    const shuffle = useCallback(() => {
+        const index = Math.floor(Math.random() * descriptions.length);
+        setNewDescript(descriptions[index]);
+    }, []);
+
+    useEffect(() => {
+        const intervalID = setInterval(shuffle, 3000);
+        return () => clearInterval(intervalID);
+    }, [shuffle])
+
 return(
   <Container fluid className="mt-5 mb-5 p-0 text-center jumbotron-image" style={{ backgroundImage:`url(${heroBackground})`, height: 500 }}>
     <Row style={{ height: 120}}></Row>
-    <Row className="">
+    <Row>
       <Col sm={4}></Col>
       <Col sm={4}>
           <h1 id="test" className='mb-3'>Alicia McNeil</h1>
-          <h4 className='mb-3'>Full Stack Web Developer focused on clean, efficient code for exceptional digital experiences.</h4>
+          <h4 className='mb-3'>Full Stack Web Developer focused on <i>{newDescript.toUpperCase()}</i> code for exceptional digital experiences.</h4>
           <a className='btn btn-outline-dark btn-lg hero-btn' href='/designthemes' role='button'>See My Work</a>
       </Col>
     </Row>
