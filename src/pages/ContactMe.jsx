cefda4f5675934eabb6c473d5f509a886ec310bd
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-10-17 22:36:39 -0400

    created state variables to clear contact form, show/hide acknowlegement modal on email submit. Also set autoreply email.

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import emailjs from 'emailjs-com'
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
+import {useState} from 'react';
+
 
 const ContactMe = () => {
+
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = () => setShowModal(false);
+
+  const [user_name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [msg, setMessage] = useState('')
+
   const sendEmail = (e) => {
     e.preventDefault()
+
+    setName('')
+    setEmail('')
+    setMessage('')
+    setShowModal(true)
+
     emailjs.sendForm(
       'service_r0pnklu',
       'template_857cdm9',
@@ -14,31 +31,70 @@ const ContactMe = () => {
       )).catch(err=> console.log(err))
   }
   return (
-    <div className="container w-25 mt-5 mb-5">
-      <h2 className="mb-3">Send a Message to Alicia</h2>
+    <>
+    <Container className="mt-5 mb-5 p-0" style={{ minHeight: "75vh"}}>
+      <Row className='mt-5'>
+        <Col sm={4}></Col>
+        <Col sm={4}><h1 className="pb-3" style={{ borderBottom: "2px solid #e84118"}}>Send a Message<br />to Alicia</h1></Col>
+        <Col sm={4}></Col>
+      </Row>
+      <Row>
+      <Col sm={4}></Col>
+      <Col sm={4}>
       <form onSubmit={sendEmail}>
         <div className="mb-3">
-          <label className="form-label" htmlFor="name">
-            Name
-          </label>
-          <input className="form-control" type="text" name="name" id="name" required />
+          <label className="form-label" htmlFor="name">Name</label>
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            id="name"
+            required
+            onChange={e => setName(e.target.value)}
+            value={user_name}
+            />
         </div>
         <div className="mb-3">
-          <label className="form-label" htmlFor="email">
-            Email
-          </label>
-          <input className="form-control" name="user_email" type="email" id="email" required />
+          <label className="form-label" htmlFor="email">Email</label>
+          <input
+            className="form-control"
+            name="user_email"
+            type="email"
+            id="email"
+            required
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+          />
         </div>
         <div className="mb-3">
-          <label className="form-label" htmlFor="message">
-            Message
-          </label>
-          <textarea className="form-control" name="message" id="message" required />
+          <label className="form-label" htmlFor="message">Message</label>
+          <textarea
+            className="form-control"
+            name="message"
+            id="message"
+            required
+            rows={7}
+            onChange={e => setMessage(e.target.value)}
+            value={msg}
+          />
         </div>
-        <button className="btn btn-outline-dark btn-lg contact-btn" type="submit">Send Email
-        </button>
+        <button className="btn btn-outline-dark btn-lg contact-btn" type="submit">Send Email</button>
       </form>
-    </div>
+      </Col>
+      <Col sm={4}></Col>
+      </Row>
+    </Container>
+
+    <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Email Sent!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Thank you for your email! I will reply within 24 hours.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="Primary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 export default ContactMe
